236de175b4d001e247c6a805b8b78458ae1b1969
root <root@smxdc1.hope.edu.kh>
2016-05-24 14:52:47 +0700

    ldoper - ported to Python3

--- ldoper.py
+++ ldoper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import argparse
 import sys
@@ -24,7 +24,7 @@ description = """
     """
 parser = argparse.ArgumentParser(description=description)
 parser.add_argument("-m", "--mode", default="raw",  help="define the mode of operation: raw, report, add ( ... yet to come: modify)")
-parser.add_argument("-s", "--search", default="objectclass=*",  help="define the search filter, default: objectclass=*")
+parser.add_argument("-s", "--search", default="(objectclass=*)",  help="define the search filter, default: objectclass=*")
 parser.add_argument("-b", "--base", default=None,  help="define the LDAP search base DN")
 parser.add_argument("-f", "--file", default=None, help="file to read the input data from; optionally stdin can be used")
 parser.add_argument("-c", "--config", default='/etc/hope/ldap.conf', help="file to read the LDAP configuratin from")
@@ -53,9 +53,9 @@ def collectSearchResults(ld, ld2, grp_p):
 def printReport(ld, mode):
     result=ld.shiftResult()
     while (result != None):
-        print  "----------------------------------"
-        print "DN: ", result.dn, "--- User Type: ", result.user_type
-        print  "----------------------------------"
+        print  ("----------------------------------")
+        print ("DN: " + result.dn + "--- User Type: " + result.user_type)
+        print  ("----------------------------------")
 
         nice_list = ['cn', 'displayname', 'mail', 'sn', 'givenname', 'samaccountname', 'memberof', 'userpassword']
         nice_list = ['displayname', 'mail','samaccountname']
@@ -65,7 +65,7 @@ def printReport(ld, mode):
         # print result.valueMap
         for id in nice_list:
             if id != None:
-                print id, ":", result.getValueList(id)
+                print (id + ":", result.getValueList(id))
         result=ld.shiftResult()
 # ---------------------------------------------------------------
 
@@ -107,7 +107,7 @@ if mode == 'add':
         for line in sys.stdin:
             inputText += line
 
-    print "Parse JSON text to Python data ------------"
+    print ("Parse JSON text to Python data ------------")
 
     jsonData = jtools.JsonData(inputText)
     mapList = jsonData.mapList
@@ -115,26 +115,33 @@ if mode == 'add':
 
     for item in mapList:
         qualMap = {}
+
+
+
+        dataObj = UserData(item)
+        qualMap = dataObj.getQualifierMap()
+        print ("Try to add to LDAP: ", qualMap)
+
         try:
-            dataObj = UserData(item)
-            qualMap = dataObj.getQualifierMap()
-            print "Try to add to LDAP: ", qualMap
-
-            if group_operation:
-                ld.addGroup(dataObj)
-            else:
-                ld.addUser(dataObj)
-
-
+            pass
         except:
-            print "ADD failed for ... ", qualMap
+            print ("ADD failed for ... ", qualMap)
+
+        if group_operation:
+            ld.addGroup(dataObj)
+        else:
+            ld.addUser(dataObj)
+
+
+        #except:
+        #    print ("ADD failed for ... ", qualMap)
 
 elif mode == 'raw':
     sys.stderr.write("LDAP search  using filter: " + args.search + "\n")
     ld.search(filter=args.search, base=args.base)
     collectSearchResults(ld, ld2, memberuid_parse)
     # print formatObj.printOut()
-    print formatObj.getJson()
+    print (formatObj.getJson())
 
 elif mode == 'report' or mode == 'fullreport':
     sys.stderr.write("LDAP search  using filter: " + args.search + "\n")
@@ -143,4 +150,4 @@ elif mode == 'report' or mode == 'fullreport':
 
 
 else:
-    print "No mode defined ..."
+    print ("No mode defined ...")
